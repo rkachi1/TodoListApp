981ee3f371e431ff369a47133f6256ab40ca69f9
rkachi1 <rangarajkk@gmail.com>
2021-05-15 18:42:13 -0400

    adding further code to improve app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  // using useStateHook to set a value for the todos
+
+  const [todos, setTodos] = useState([
+    "Take gos for a walk",
+    "Get hired in a company",
+    "Need something in replacement",
+  ]);
+  // using useStateHook to set a value for the input
+  const [input, setInput] = useState("");
+
+  //Adding a todo function
+  const addTodos = (event) => {
+    event.preventDefault();
+    setTodos([...todos, input]);
+    setInput("");
+  };
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <div className="App">
+        <h1>Hello World</h1>
+        <form>
+          <input
+            placeholder="Add todo..."
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          ></input>
+          <button type="submit" onClick={addTodos}>
+            Add Todo
+          </button>
+        </form>
+      </div>
+      <div className="newClass">
+        <ul>
+          {todos.map((todo) => {
+            return <li>{todo}</li>;
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
